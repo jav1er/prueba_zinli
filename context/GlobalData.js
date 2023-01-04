0500d0d6ab7e1d0e6be3dc44c390fefa7984aa65
jrangel-payall <jrangelpayall@gmail.com>
2023-01-04 19:53:05 -0400

    Configura useContext

---
+++ context/GlobalData.js
@@ -0,0 +1,6 @@
+import { createContext } from "react"
+
+export const GlobalData = createContext({
+  formOneData: null,
+  setFormOneData: () => null,
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+
+import { useMemo, useState, useEffect } from "react"
+import { GlobalData } from "../context/GlobalData"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [formOneData, setFormOneData] = useState("");
+
+  const formsData = useMemo(
+    (_) => ({
+      formOneData,
+    }),
+    [formOneData]
+  );
+
+  return (
+    <GlobalData.Provider value={formsData}>
+      <Component {...pageProps} />
+    </GlobalData.Provider>
+  );
 }
