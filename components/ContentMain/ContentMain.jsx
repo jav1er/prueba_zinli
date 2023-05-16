5ad9316f69200f7eb09175392d855d111853f8a6
jarvis rangel <jarvijavier@gmail.com>
2023-05-15 23:09:34 -0400

    Agrega formulario de guia

--- components/ContentMain/ContentMain.jsx
+++ components/ContentMain/ContentMain.jsx
@@ -1,14 +1,34 @@
-import { useEffect, useState } from "react"
-import Post from "../Post/Post"
+import { useEffect, useState } from "react";
+import Post from "../Post/Post";
 
 export default function ContentMain() {
-  const [products, setProducts] = useState([])
-  const [filter, setFilter] = useState("")
+  const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState("");
 
- 
+  const getProducts = async () => {
+    await fetch(
+      `https://5eed24da4cbc340016330f0d.mockapi.io/api/articles?filter=${filter}`
+    )
+      .then((response) => response.json())
+      .then((response) => setProducts(response))
+      .catch((error) => error);
+  };
+
+  const handleFilter = (filter) => {
+    setFilter(filter);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [filter]);
+
   return (
     <section className="contentMain">
-    
+      <div className="container">
+        <div>
+          <h2 className="title">Login </h2>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
